8bb35fc665d9fb82c3cd4de6ffbe251f6ecf0d74
Jake K <111465068+xatikat@users.noreply.github.com>
2025-07-30 21:42:34 -0600

    fix loading screen

--- js/onload.js
+++ js/onload.js
@@ -1,14 +1,17 @@
 import { rotateDecorations } from './decor-animation.js'
 
+let loadingDiv = document.getElementById('loading');
+
+setTimeout(() => {loadingDiv.children[0].style.opacity = 1;}, 1000)
+
 window.onload = function() {
     //let main = document.getElementsByTagName('main');
     //main[0].style.opacity = 1;
     let decorationsDiv = document.getElementById('decorations');
     //decorationsDiv.style.opacity = 1;
-    let loadingDiv = document.getElementById('loading');
+    
     loadingDiv.style.opacity = 0;
-
-    setTimeout(() => {loadingDiv.children[0].style.opacity = 1}, 1000)
+    setTimeout(() => {loadingDiv.style.display = 'none';}, 800)
     
     // start interval for rotating decorations!!
     setInterval(() => {rotateDecorations(decorationsDiv.getElementsByTagName('img'))}, 2000);
